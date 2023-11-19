767f4e74adfd48830f5056b5287618d15ca93c9c
AxelRaboit <axel.raboit@gmail.com>
2023-11-19 21:50:41 +0100

    [ADD] Style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\City;
 use App\Entity\Country;
+use App\Repository\CityRepository;
+use App\Repository\CountryRepository;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -19,12 +22,25 @@ class HomeController extends AbstractController
         $form = $this->createFormBuilder()
             ->add('name', TextType::class)
             ->add('country', EntityType::Class, [
+                'placeholder' => 'Select a country',
                 'class' => Country::class,
-                'choice_label' => 'name',
+                'choice_label' => (function(Country $country) {
+                    return $country->getId() . ' - ' . $country->getName();
+                }),
+                'query_builder' => function(CountryRepository $countryRepository) {
+                    return $countryRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
             ])
             ->add('city', EntityType::Class, [
+                'placeholder' => 'Select a city',
+                'disabled' => true,
                 'class' => City::class,
                 'choice_label' => 'name',
+                'query_builder' => function(CityRepository $cityRepository) {
+                    return $cityRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
             ])
             ->add('message', TextareaType::class)
             ->getForm();
