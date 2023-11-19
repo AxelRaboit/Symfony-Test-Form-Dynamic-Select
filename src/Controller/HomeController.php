6884107299e394856e9b1689a96f552e3ea0b2b5
AxelRaboit <axel.raboit@gmail.com>
2023-11-19 21:35:40 +0100

    [ADD] FormType

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
+use App\Entity\Country;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +16,19 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('country', EntityType::Class, [
+                'class' => Country::class,
+                'choice_label' => 'name',
+            ])
+            ->add('city', EntityType::Class, [
+                'class' => City::class,
+                'choice_label' => 'name',
+            ])
+            ->add('message', TextareaType::class)
+            ->getForm();
+
+        return $this->render('home.html.twig', compact('form'));
     }
 }
