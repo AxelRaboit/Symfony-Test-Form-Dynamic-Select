3b1c673142c60edaeed8eebc3ad62ea939ce215b
AxelRaboit <axel.raboit@gmail.com>
2023-11-19 21:58:51 +0100

    [UPDATE] Improve form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,17 +11,28 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form = $this->createFormBuilder()
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your name']),
+                    new Length(['min' => 3]),
+                ],
+            ])
             ->add('country', EntityType::Class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please select a country']),
+                ],
                 'placeholder' => 'Select a country',
                 'class' => Country::class,
                 'choice_label' => (function(Country $country) {
@@ -33,6 +44,9 @@ class HomeController extends AbstractController
                 },
             ])
             ->add('city', EntityType::Class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please select a city']),
+                ],
                 'placeholder' => 'Select a city',
                 'disabled' => true,
                 'class' => City::class,
@@ -42,8 +56,20 @@ class HomeController extends AbstractController
                         ->orderBy('c.name', 'ASC');
                 },
             ])
-            ->add('message', TextareaType::class)
+            ->add('message', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your message']),
+                    new Length(['min' => 10]),
+                ],
+            ])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            dd($data);
+        }
 
         return $this->render('home.html.twig', compact('form'));
     }
